6e966f6077cd3ca0cb49e5abfd12c5256673034c
Erfan Pro97 <ghaziasgar.seyederfan@gmail.com>
2024-01-08 12:37:47 +0330

    Add minimax alpha-beta pruning algorithm

--- multiAgents.py
+++ multiAgents.py
@@ -12,10 +12,9 @@
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
 
 
-from util import manhattanDistance
-from game import Directions
-import random, util
+from math import inf
 
+import util
 from game import Agent
 from pacman import GameState
 
@@ -30,8 +29,33 @@ def scoreEvaluationFunction(currentGameState: GameState):
     return currentGameState.getScore()
 
 
+def betterEvaluationFunction(currentGameState: GameState):
+    """
+    Your extreme ghost-hunting, pellet-nabbing, food-gobbling, unstoppable
+    evaluation function (question 5).
+
+    DESCRIPTION: <write something here so we know what you did>
+    """
+
+    pos = currentGameState.getPacmanPosition()
+    food = currentGameState.getFood()
+    ghostStates = currentGameState.getGhostStates()
+
+    foodDist = [util.manhattanDistance(pos, f) for f in food.asList()]
+    ghostDist = [util.manhattanDistance(
+        pos, g.getPosition()) for g in ghostStates]
+
+    if len(foodDist) == 0:
+        return 0
+    if min(ghostDist) < 2:
+        return -inf
+
+    return currentGameState.getScore() - min(foodDist) - min(ghostDist)
+
+
 class MultiAgentSearchAgent(Agent):
-    def __init__(self, evalFn="scoreEvaluationFunction", depth="2", time_limit="6"):
+    def __init__(self, evalFn="scoreEvaluationFunction", depth="2",
+                 time_limit="6"):
         self.index = 0  # Pacman is always agent index 0
         self.evaluationFunction = util.lookup(evalFn, globals())
         self.depth = int(depth)
@@ -41,7 +65,8 @@ class MultiAgentSearchAgent(Agent):
 class AIAgent(MultiAgentSearchAgent):
     def getAction(self, gameState: GameState):
         """
-        Here are some method calls that might be useful when implementing minimax.
+        Here are some method calls that might be useful when implementing
+        minimax.
 
         gameState.getLegalActions(agentIndex):
         Returns a list of legal actions for an agent
@@ -60,9 +85,34 @@ class AIAgent(MultiAgentSearchAgent):
         Returns whether or not the game state is a losing state
         """
 
-        # TODO: Your code goes here
-        # util.raiseNotDefined()
+        def alphaBeta(state, depth, alpha, beta, agent):
+            isMax = agent == 0
+            nextDepth = depth-1 if isMax else depth
+            if nextDepth == 0 or state.isWin() or state.isLose():
+                return self.evaluationFunction(state), None
 
-        def minimax()
+            nextAgent = (agent + 1) % state.getNumAgents()
+            bestVal = -inf if isMax else inf
+            bestAction = None
+            bestOf = max if isMax else min
 
+            for action in state.getLegalActions(agent):
+                successorState = state.generateSuccessor(agent, action)
+                valOfAction, _ = alphaBeta(
+                    successorState, nextDepth, alpha, beta, nextAgent)
+                if bestOf(bestVal, valOfAction) == valOfAction:
+                    bestVal, bestAction = valOfAction, action
 
+                if isMax:
+                    if bestVal > beta:
+                        return bestVal, bestAction
+                    alpha = max(alpha, bestVal)
+                else:
+                    if bestVal < alpha:
+                        return bestVal, bestAction
+                    beta = min(beta, bestVal)
+
+            return bestVal, bestAction
+
+        _, action = alphaBeta(gameState, self.depth+1, -inf, inf, self.index)
+        return action
